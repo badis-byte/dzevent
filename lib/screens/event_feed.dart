01ad65b790e1715bb04fc9e8e12dd00fe91cf5b3
aliou-prog <alimokhati2023@gmail.com>
2025-10-21 15:32:33 +0100

    feat: add appbar and searchbar

--- lib/screens/event_feed.dart
+++ lib/screens/event_feed.dart
@@ -1,3 +1,4 @@
+import 'package:dzevent/lib/styles.dart';
 import 'package:dzevent/screens/home.dart';
 import 'package:flutter/material.dart';
 
@@ -8,8 +9,33 @@ class EventFeed extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Sample App")),
-      body: Column(children: [NavScreenLink()]),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                IconButton(onPressed: () {}, icon: Icon(Icons.list)),
+                Expanded(
+                  child: Text(
+                    "Upcoming events",
+                    textAlign: TextAlign.center,
+                    style: headingStyle,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.notifications_none),
+                ),
+              ],
+            ),
+            SearchAnchor.bar(
+              suggestionsBuilder: (context, controller) => [],
+              barHintText: "Search for events ...",
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
