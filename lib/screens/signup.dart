0a6e840382917476e602a738d3379c3c17143a5e
abdelaliMokhati <abdelali.mokhati@ensia.edu.dz>
2025-10-26 17:31:09 +0100

    style: formatting

--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -21,19 +21,38 @@ class _SignupState extends State<Signup> {
             //mainAxisAlignment: MainAxisAlignment.center,
             children: [
               SizedBox(height: 15),
-              Text("Create New Account", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-              SizedBox(height: 20,),
+              Text(
+                "Create New Account",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 20),
               googleButton(),
-              SizedBox(height: 20,),
+              SizedBox(height: 20),
               orDevider(),
-              SizedBox(height: 35,),
-              buildInput(label: "Full Name", hint: "Enter your full name", pass: false),
-              SizedBox(height: 20,),
-              buildInput(label: "Email Address", hint: "Enter your email address", pass: false),
-              SizedBox(height: 20,),
-              buildInput(label: "Password", hint: "Enter your password", pass: true),
-              SizedBox(height: 20,),
-              buildInput(label: "Confirm Password", hint: "Confirm your password", pass: true),
+              SizedBox(height: 35),
+              buildInput(
+                label: "Full Name",
+                hint: "Enter your full name",
+                pass: false,
+              ),
+              SizedBox(height: 20),
+              buildInput(
+                label: "Email Address",
+                hint: "Enter your email address",
+                pass: false,
+              ),
+              SizedBox(height: 20),
+              buildInput(
+                label: "Password",
+                hint: "Enter your password",
+                pass: true,
+              ),
+              SizedBox(height: 20),
+              buildInput(
+                label: "Confirm Password",
+                hint: "Confirm your password",
+                pass: true,
+              ),
               SizedBox(height: 30),
               createButton(),
               SizedBox(height: 30),
@@ -50,101 +69,106 @@ class _SignupState extends State<Signup> {
 
   Row yesAccount() {
     return Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Already have an account?",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 15,
-                    decoration: TextDecoration.none,
-                  ),
-                ),
-                Text(
-                  " Log In",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.blue,
-                    fontSize: 15,
-                    decoration: TextDecoration.none,
-                  ),
-                ),
-              ],
-            );
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          "Already have an account?",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 15,
+            decoration: TextDecoration.none,
+          ),
+        ),
+        Text(
+          " Log In",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.blue,
+            fontSize: 15,
+            decoration: TextDecoration.none,
+          ),
+        ),
+      ],
+    );
   }
 
   Wrap policy() {
     return Wrap(
-              alignment: WrapAlignment.center,
-              children: [
-                Text(
-                  "By creating an account, you agree to our",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 15,
-                    decoration: TextDecoration.none,
-                  ),
-                ),
-                Text(
-                  " Terms of Service",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.blue,
-                    fontSize: 15,
-                    decoration: TextDecoration.none,
-                  ),
-                ),
-                Text(
-                  " and",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 15,
-                    decoration: TextDecoration.none,
-                  ),
-                ),
-                Text(
-                  " Privacy Policy",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.blue,
-                    fontSize: 15,
-                    decoration: TextDecoration.none,
-                  ),
-                ),
-
-              ],
-            );
+      alignment: WrapAlignment.center,
+      children: [
+        Text(
+          "By creating an account, you agree to our",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 15,
+            decoration: TextDecoration.none,
+          ),
+        ),
+        Text(
+          " Terms of Service",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.blue,
+            fontSize: 15,
+            decoration: TextDecoration.none,
+          ),
+        ),
+        Text(
+          " and",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 15,
+            decoration: TextDecoration.none,
+          ),
+        ),
+        Text(
+          " Privacy Policy",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.blue,
+            fontSize: 15,
+            decoration: TextDecoration.none,
+          ),
+        ),
+      ],
+    );
   }
 
   Container createButton() {
     return Container(
-              margin: EdgeInsets.only(left: 25, right: 25),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color.fromARGB(255,0,51,102,), // Button color
-                  foregroundColor: Colors.white, // Text color
-                  minimumSize: const Size(400, 60), // Width x Height
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 24,
-                    vertical: 12,
-                  ),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15), // Rounded corners
-                  ),
-                  elevation: 5, // Shadow depth
-                ),
-                onPressed: () {
-                  print('create account button pressed!');
-                },
-                child: const Text('Create Account', style: TextStyle(fontSize: 16)),
-              ),
-            );
+      margin: EdgeInsets.only(left: 25, right: 25),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: const Color.fromARGB(
+            255,
+            0,
+            51,
+            102,
+          ), // Button color
+          foregroundColor: Colors.white, // Text color
+          minimumSize: const Size(400, 60), // Width x Height
+          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15), // Rounded corners
+          ),
+          elevation: 5, // Shadow depth
+        ),
+        onPressed: () {
+          print('create account button pressed!');
+        },
+        child: const Text('Create Account', style: TextStyle(fontSize: 16)),
+      ),
+    );
   }
 
-  Column buildInput({required String label, required String hint, required bool pass}) {
+  Column buildInput({
+    required String label,
+    required String hint,
+    required bool pass,
+  }) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -185,75 +209,64 @@ class _SignupState extends State<Signup> {
 
   Row orDevider() {
     return Row(
-              children: const [
-                Expanded(
-                  child: Divider(
-                    color: Colors.grey,
-                    thickness: 1,
-                    indent: 25, // left spacing
-                    endIndent: 10, // space before "OR"
-                  ),
-                ),
-                Text(
-                  "or",
-                  style: TextStyle(
-                    color: Color.fromARGB(255, 109, 107, 107),
-                    fontWeight: FontWeight.normal,
-                  ),
-                ),
-                Expanded(
-                  child: Divider(
-                    color: Colors.grey,
-                    thickness: 1,
-                    indent: 10, // space after "OR"
-                    endIndent: 25,
-                  ),
-                ),
-              ],
-            );
+      children: const [
+        Expanded(
+          child: Divider(
+            color: Colors.grey,
+            thickness: 1,
+            indent: 25, // left spacing
+            endIndent: 10, // space before "OR"
+          ),
+        ),
+        Text(
+          "or",
+          style: TextStyle(
+            color: Color.fromARGB(255, 109, 107, 107),
+            fontWeight: FontWeight.normal,
+          ),
+        ),
+        Expanded(
+          child: Divider(
+            color: Colors.grey,
+            thickness: 1,
+            indent: 10, // space after "OR"
+            endIndent: 25,
+          ),
+        ),
+      ],
+    );
   }
 
   Container googleButton() {
     return Container(
-              margin: EdgeInsets.only(left: 25, right: 25),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.white, // Button color
-                  foregroundColor: Colors.black, // Text color
-                  minimumSize: const Size(400, 50), // Width x Height
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 24,
-                    vertical: 12,
-                  ),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(25), // Rounded corners
-                    side: BorderSide(
-                      color: const Color.fromARGB(255, 205, 204, 204), // Border color
-                      width: 1, // Border width
-                    ),
-                  ),
-                  //elevation: 5,                           // Shadow depth
-                ),
-                onPressed: () {
-                  print('google button pressed!');
-                },
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset(
-                      "assets/images/google1.png",
-                      height: 35,
-                      width: 35,
-                    ),
-                    SizedBox(width: 10),
-                    Text(
-                      'Continue with Google',
-                      style: TextStyle(fontSize: 16),
-                    ),
-                  ],
-                ),
-              ),
-            );
+      margin: EdgeInsets.only(left: 25, right: 25),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.white, // Button color
+          foregroundColor: Colors.black, // Text color
+          minimumSize: const Size(400, 50), // Width x Height
+          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(25), // Rounded corners
+            side: BorderSide(
+              color: const Color.fromARGB(255, 205, 204, 204), // Border color
+              width: 1, // Border width
+            ),
+          ),
+          //elevation: 5,                           // Shadow depth
+        ),
+        onPressed: () {
+          print('google button pressed!');
+        },
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("assets/images/google1.png", height: 35, width: 35),
+            SizedBox(width: 10),
+            Text('Continue with Google', style: TextStyle(fontSize: 16)),
+          ],
+        ),
+      ),
+    );
   }
-
 }
