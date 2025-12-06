1ad8356009418dcd020c93831a269819c3dc91e2
chakib hamada <chakib.hamada@ensia.edu.dz>
2025-12-06 12:43:22 +0100

    fixed issuees type shitt 4 and fixed my problem

--- lib/data/databases/db_base.dart
+++ lib/data/databases/db_base.dart
@@ -4,13 +4,13 @@ import 'package:sqflite/sqflite.dart';
 import 'dbhelper.dart';
 
 class DBBaseTable {
-  var dbTable = 'TABLE_NAME_MUST_OVERRIDE';
+  var db_table = 'TABLE_NAME_MUST_OVERRIDE';
 
   Future<bool> insertRecord(Map<String, dynamic> data) async {
     try {
       final database = await DBHelper.getDatabase();
       database.insert(
-        dbTable,
+        db_table,
         data,
         conflictAlgorithm: ConflictAlgorithm.replace,
       );
@@ -24,7 +24,7 @@ class DBBaseTable {
   Future<bool> deleteRecord(String id) async {
     try {
       final db = await DBHelper.getDatabase();
-      await db.delete(dbTable, where: 'id = ?', whereArgs: [id]);
+      await db.delete(db_table, where: 'id = ?', whereArgs: [id]);
       return true;
     } on Exception catch (e, stacktrace) {
       print('$e --> $stacktrace');
@@ -36,7 +36,7 @@ class DBBaseTable {
     try {
       final database = await DBHelper.getDatabase();
       var data = await database.query(
-        dbTable,
+        db_table,
         where: 'id = ?',
         whereArgs: [id],
       );
@@ -51,7 +51,7 @@ class DBBaseTable {
     try {
       final database = await DBHelper.getDatabase();
       var data = await database.rawQuery(
-        "select * from $dbTable order by id DESC",
+        "select * from $db_table order by id DESC",
       );
       return data;
     } catch (e, stacktrace) {
@@ -63,7 +63,7 @@ class DBBaseTable {
   Future<bool> deleteRecords() async {
     try {
       final db = await DBHelper.getDatabase();
-      await db.delete(dbTable);
+      await db.delete(db_table);
       return true;
     } on Exception catch (e, stacktrace) {
       print('$e --> $stacktrace');
@@ -75,7 +75,7 @@ class DBBaseTable {
     try {
       final db = await DBHelper.getDatabase();
       await db.update(
-        dbTable,
+        db_table,
         data,
         where: 'id = ?',
         whereArgs: [id],
