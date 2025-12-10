54fd2a3fb0afcbf0e1a5cc473034bf990fd72e2f
BadisBelkessam <badis.belkessam@ensia.edu.dz>
2025-12-10 06:56:54 +0100

    a little check up and a little modifications of something here and something there u know

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'dart:io';
 import 'package:dzevent/logic/cubits/events/events_cubit.dart';
 import 'package:dzevent/logic/cubits/interests/interests_cubit.dart';
 import 'package:dzevent/presentation/screens/assocAdmin.dart';
+import 'package:dzevent/presentation/screens/associationProfileTwo.dart';
 import 'package:dzevent/presentation/screens/event_feed.dart';
 import 'package:dzevent/presentation/screens/login.dart';
 import 'package:dzevent/presentation/screens/signup.dart';
@@ -54,6 +55,7 @@ class MainApp extends StatelessWidget {
           '/signup': (_) => Signup(),
           '/login': (_) => Login(),
           '/event_feed': (_) => EventFeed(),
+          '/assocownprofile': (_) => AssocProfTwo(),
         },
       ),
     );
